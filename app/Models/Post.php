0fe477d39cd6ac4795fa6ee9212c555b02b8c404
kazuma-shinomiya <kazuma.s6145@icloud.com>
2023-08-17 21:29:40 +0900

    リレーション

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -14,7 +14,14 @@ class Post extends Model
     protected $fillable = [
         'title',
         'body',
+        'category_id'
     ];
+    
+    //「1対多」の関係なので単数系に
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
     
     public function getByLimit(int $limit_count = 10)
     {
@@ -25,6 +32,6 @@ class Post extends Model
     public function getPaginateByLimit(int $limit_count = 10)
     {
       // updated_atで降順に並べたあと、limitで件数制限をかける
-      return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
+      return $this::with('category')->orderBy('updated_at', 'DESC')->paginate($limit_count);
     }
 }
